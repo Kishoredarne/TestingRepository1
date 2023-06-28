71c8931f4e72b271e9c7886bd2aa79c376391d4f
Mahendra Dubey <mahendra.dubey@necsws.com>
2023-06-28 16:25:25 +0530

    Initial Commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @author mahendra.dubey
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Mahendra Krishan Dubey");
+		System.out.println("Hello Mahendra !!");
+
+	}
+
+}
